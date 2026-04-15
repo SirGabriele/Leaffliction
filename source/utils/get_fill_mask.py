8fb11c20c23ce2b9b4866af7f2d4c3b3c04613d6
arthurfaby <arthurfaby1@gmail.com>
2026-04-15 16:17:02 +0200

    refactor(transformation): remove unused code and comments in get_fill_mask.py

--- source/utils/get_fill_mask.py
+++ source/utils/get_fill_mask.py
@@ -9,14 +9,3 @@ def get_fill_mask(image: np.ndarray) -> np.ndarray:
     mask_clean = plantcv.fill(bin_img=mask, size=50)
 
     return mask_clean
-    # image_without_background = rembg.remove(image)
-
-    # Grayscale image of the light colorspace channel
-    # image_grayscale = plantcv.rgb2gray_lab(
-    #     rgb_img=image_without_background, channel='l'
-    # )
-
-    # Binary mask on threshold values targeting light objects
-    # return plantcv.threshold.otsu(
-    #     gray_img=image_grayscale, object_type="light"
-    # )
