a51d1c8ad514511ca4e87f7492eb558fd64c790e
arthurfaby <arthurfaby1@gmail.com>
2026-04-15 15:44:04 +0200

    refactor(transformation): improve argument handling and formatting in Transformation.py

--- Transformation.py
+++ Transformation.py
@@ -15,6 +15,7 @@ from concurrent.futures import ProcessPoolExecutor, as_completed
 
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
+
 def argparse_init() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser(
         prog="Transformation.py",
@@ -37,25 +38,39 @@ def argparse_init() -> argparse.ArgumentParser:
     parser.add_argument("-dst", "--destination",
                         help="Destination directory")
 
-    parser.add_argument("-m", "--mask", action="store_true", help="Apply mask to the image transformation")
-    parser.add_argument("-s", "--saturation", action="store_true", help="Apply saturation to the image transformation")
-    parser.add_argument('-r', '--roi', action="store_true", help="Apply region of interest to the image transformation")
-    parser.add_argument('-a', '--analysis', action="store_true", help="Apply analysis to the image transformation")
-    parser.add_argument('-p', '--pseudolandmark', action="store_true", help="Apply pseudolandmark to the image transformation")
-    parser.add_argument('-e', '--edges', action="store_true", help="Apply edges detection to the image transformation")
+    parser.add_argument("-m", "--mask", action="store_true",
+                        help="Apply mask to the image transformation")
+    parser.add_argument("-s", "--saturation", action="store_true",
+                        help="Apply saturation to the image transformation")
+    parser.add_argument('-r', '--roi', action="store_true",
+                        help="Apply region of interest to the image transformation")
+    parser.add_argument('-a', '--analysis', action="store_true",
+                        help="Apply analysis to the image transformation")
+    parser.add_argument('-p', '--pseudolandmark', action="store_true",
+                        help="Apply pseudolandmark to the image transformation")
+    parser.add_argument('-e', '--edges', action="store_true",
+                        help="Apply edges detection to the image transformation")
     return parser
 
-def validate_arguments_or_fail(args: argparse.Namespace, parser: argparse.ArgumentParser) -> None:
+
+def validate_arguments_or_fail(args: argparse.Namespace,
+                               parser: argparse.ArgumentParser) -> None:
     if args.image_file and (args.source or args.destination):
-        parser.error("You cannot provide both an image file and source/destination directories.")
-    if (not args.destination and args.source) or (args.destination and not args.source):
-        parser.error("Both source and destination directories must be provided together.")
+        parser.error(
+            "You cannot provide both an image file and source/destination directories.")
+    if (not args.destination and args.source) or (
+            args.destination and not args.source):
+        parser.error(
+            "Both source and destination directories must be provided together.")
     if not args.image_file and not (args.source and args.destination):
-        parser.error("You must provide either a single image file or both source and destination directories.")
+        parser.error(
+            "You must provide either a single image file or both source and destination directories.")
 
 
-def process_single_image_worker(rel_path: Path, source: Path, destination: Path, args: argparse.Namespace) -> tuple[
-    bool, str]:
+def process_single_image_worker(rel_path: Path, source: Path,
+                                destination: Path, args: argparse.Namespace) -> \
+        tuple[
+            bool, str]:
     try:
         image_file_path: Path = source / rel_path
         image: np.ndarray = load_image(image_file_path)
@@ -88,23 +103,32 @@ def process_single_image_worker(rel_path: Path, source: Path, destination: Path,
         return False, f"Error with {rel_path}: {str(e)}"
 
 
-def handle_single_file_mode(args: argparse.Namespace) -> None:
-    print(f"Single file mode : src {args.image_file}")
-    image_file_path: Path = Path(args.image_file)
+def handle_single_file_mode(
+        image_file: str,
+        saturation: bool = False,
+        mask: bool = False,
+        roi: bool = False,
+        analysis: bool = False,
+        pseudolandmark: bool = False,
+        edges: bool = False) -> None:
+    print(f"Single file mode : src {image_file}")
+    image_file_path: Path = Path(image_file)
     image: np.ndarray = load_image(image_file_path)
     transformed_images = transform_image(
         image,
-        saturation=args.saturation,
-        mask=args.mask,
-        roi=args.roi,
-        analysis=args.analysis,
-        pseudolandmark=args.pseudolandmark,
-        edges=args.edges
+        saturation=saturation,
+        mask=mask,
+        roi=roi,
+        analysis=analysis,
+        pseudolandmark=pseudolandmark,
+        edges=edges
     )
     display_images(image, transformed_images)
 
+
 def handle_batch_mode(args: argparse.Namespace) -> None:
-    print(f"Batch mode :\n\t- Source = {args.source}\n\t- Destination = {args.destination}")
+    print(
+        f"Batch mode :\n\t- Source = {args.source}\n\t- Destination = {args.destination}")
     source_path = Path(args.source)
     dest_path = Path(args.destination)
     valid_extensions = {'.jpg', '.jpeg', '.png'}
@@ -123,13 +147,16 @@ def handle_batch_mode(args: argparse.Namespace) -> None:
         for img_path in images_to_process:
             rel_path = img_path.relative_to(source_path)
             futures.append(
-                executor.submit(process_single_image_worker, rel_path, source_path, dest_path, args)
+                executor.submit(process_single_image_worker, rel_path,
+                                source_path, dest_path, args)
             )
 
-        for future in tqdm(as_completed(futures), total=len(images_to_process), desc="Processing images", unit="img"):
+        for future in tqdm(as_completed(futures), total=len(images_to_process),
+                           desc="Processing images", unit="img"):
             success, result_message = future.result()
             if not success:
                 tqdm.write(result_message)
+
 
 def main():
     parser: argparse.ArgumentParser = argparse_init()
@@ -137,7 +164,11 @@ def main():
     validate_arguments_or_fail(args, parser)
 
     if args.image_file:
-        handle_single_file_mode(args)
+        handle_single_file_mode(args.image_file, saturation=args.saturation,
+                                mask=args.mask, roi=args.roi,
+                                analysis=args.analysis,
+                                pseudolandmark=args.pseudolandmark,
+                                edges=args.edges)
     else:
         handle_batch_mode(args)
 
