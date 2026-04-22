d8a164ab6aca542198ae185b27b166bf0ea81670
pirabaud <rabaudp@gmail.com>
2026-04-22 08:56:43 +0200

    feat: print class predicted

--- predict.py
+++ predict.py
@@ -8,36 +8,35 @@ import numpy as np
 from matplotlib import pyplot as plt
 from source.load_image import load_image
 from source.transform_image import transform_image
+from source.utils.valid_file import valid_file
 
-
-from source.utils.existing_directory import existing_directory
 
 def argparse_init() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser(
         description='Run training',
-        usage="python3.13 train.py <directory>"
+        usage="./venv/bin/python3.10 predict.py <image file>"
     )
     parser.add_argument(
-        'directory',
-        help='Path to the directory file'
+        'image_file',
+        type=valid_file,
+        help='Path to the image file'
     )
     return parser
 
 
 def main():
     args = argparse_init().parse_args()
-    img = load_image(args.directory)
+    img = load_image(args.image_file)
 
     plt.subplot(1, 2, 1)
     plt.imshow(img)
-    plt.title("Original")
     plt.axis('off')
 
     transformed_dict = transform_image(img, background_removal=True)
 
     key_roi = "Background removal"
     if key_roi not in transformed_dict:
-        print(f"Erreur : La transformation '{key_roi}' a échoué.")
+        print(f"Error : transformation '{key_roi}' failed.")
         return
 
     img_transformed_rgb = transformed_dict[key_roi]
@@ -49,30 +48,23 @@ def main():
     img_ready = cv2.resize(img_transformed_rgb, (128, 128))
     img_normalized = img_ready.astype("float32")
     img_final = np.expand_dims(img_normalized, axis=0)
-    predictions = model.predict(img_final, verbose=0)  # verbose=0 pour pas de logs inutiles
-
-    # 4. INTERPRÉTER LE RÉSULTAT
-    # predictions est un tableau de probabilités, on prend l'indice le plus élevé
+    predictions = model.predict(img_final, verbose=0)
     class_idx = np.argmax(predictions[0])
 
-    # --- LECTURE DU JSON ---
     try:
         with open("class_names.json", "r") as f:
             class_names = json.load(f)
     except FileNotFoundError:
-        print("Erreur : Le fichier 'class_names.json' est introuvable.")
-        print("Assure-toi de l'avoir généré lors de l'entraînement.")
+        print("Error : file 'class_names.json' not found.")
         return
 
-    # On récupère le nom de la maladie
-    resultat = class_names[class_idx]
+    class_name = class_names[class_idx]
 
-    # Bonus : On calcule le pourcentage de certitude de l'IA
-    confiance = 100 * np.max(predictions[0])
-
-    print(f"Maladie détectée : {resultat} (Confiance : {confiance:.2f}%)")
-
-    plt.show()
+    plt.figtext(0.5,
+                0.15,
+                f"Class predicted: {class_name}",
+                horizontalalignment='center',
+                fontsize=20)
 
     plt.show()
     exit(0)
